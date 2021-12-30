3324a825e0c34a58908a5a404a600edecb947221
Subramaniyam Dharmeshwaran <dhu2cob@bosch.com>
2021-12-30 19:55:48 +0530

    feat:some features

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     branches: "master",
     repositoryUrl: "https://github.com/dharmidharmu/ci-cd.git",
-        "plugins": ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/npm"]
+        "plugins": ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/github"]
 }
